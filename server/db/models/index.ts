f3559e92bf242c3ff40bbdefae27a183198bbd43
madisoncarr <madison.carr@utexas.edu>
2019-10-20 15:12:58 -0500

    adds associations

--- server/db/models/index.ts
+++ server/db/models/index.ts
@@ -4,4 +4,9 @@ import Plant from '../models/plant/plant';
 import PlantSubscription from '../models/plantSubscription/plantSubscription';
 import User from '../models/user/user';
 
+User.belongsToMany(Habitat, { through: HabitatSubscription });
+Habitat.belongsToMany(User, { through: HabitatSubscription });
+User.belongsToMany(Plant, { through: PlantSubscription });
+Plant.belongsToMany(User, { through: PlantSubscription });
+
 export { Habitat, HabitatSubscription, Plant, PlantSubscription, User };
